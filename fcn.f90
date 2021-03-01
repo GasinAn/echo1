4337f92844ea26e8f0f4cff4fe5ab0e52e28c761
GasinAn <Gasin185@163.com>
2021-03-01 23:07:44 +0800

    merge fe+fp, set mode without SRP

--- fcn.f90
+++ fcn.f90
@@ -1,7 +1,5 @@
 
-    subroutine fcn(me,x,y,f)
-
-    !! subroutine computing the value of \(dy/dx=f(x,y)\)
+    subroutine fcn(me,t,pv,vf)
 
     use dop853_module
     use dop853_constants
@@ -9,15 +7,28 @@
     implicit none
 
     class(dop853_class),intent(inout) :: me
-    !! dop853_class object
-    real(wp),intent(in)               :: x
-    !! independent variable \(x\)
-    real(wp),dimension(:),intent(in)  :: y
-    !! state vector \(y(x)\)
-    real(wp),dimension(:),intent(out) :: f
-    !! derivative vector \(f(x,y)=dy/dx\)
+    real(wp),intent(in)               :: t
+    real(wp),dimension(:),intent(in)  :: pv
+    real(wp),dimension(:),intent(out) :: vf
 
-    f(1:3)=y(4:6)
-    f(4:6)=-y(1:3)/sum(y(1:3)**2.0_wp)**1.5_wp
+    integer,parameter :: n_max = 100
+    real(wp),dimension(0:n_max,0:n_max) :: c_matrix, s_matrix
+    real(wp),dimension(1:n_max,0:n_max-1) :: a
+    real(wp),dimension(1:n_max) :: b
+    real(wp),dimension(2:n_max,0:n_max-2) :: c
+    real(wp),dimension(2:n_max) :: d
+    real(wp),dimension(2:n_max,0:n_max-1) :: e
+    real(wp),dimension(0:n_max,0:n_max) :: p
+    real(wp),dimension(3) :: fe
+    real(wp),dimension(3) :: fp
+
+    call readegm(n_max, c_matrix, s_matrix)
+    call getabcdep(n_max, a, b, c, d, e, p)
+    call getfe(t, pv(1), pv(2), pv(3), &
+               n_max, a, b, c, d, e, c_matrix, s_matrix, p, fe)
+    call getfp(2400000.5D0+t, pv(1:3), fp)
+
+    vf(1:3) = pv(4:6)
+    vf(4:6) = fe+fp
 
     end subroutine fcn
--- main.f90
+++ main.f90
@@ -1,61 +1,33 @@
 
     program main
-!-------------------------------------------------------------------------------
+
     use dop853_module
     use dop853_constants
 
     use easydop853_module
-!-------------------------------------------------------------------------------
+
     implicit none
 
     integer,parameter :: len_data_1961 = 152
     real(wp),dimension(len_data_1961) :: t_1961
     real(wp),dimension(3,len_data_1961) :: pxyz_1961, vxyz_1961
 
-    integer,parameter :: n_max = 100
-    real(wp),dimension(0:n_max,0:n_max) :: c_matrix, s_matrix
-
-    real(wp),dimension(1:n_max,0:n_max-1) :: a
-    real(wp),dimension(1:n_max) :: b
-    real(wp),dimension(2:n_max,0:n_max-2) :: c
-    real(wp),dimension(2:n_max) :: d
-    real(wp),dimension(2:n_max,0:n_max-1) :: e
-    real(wp),dimension(0:n_max,0:n_max) :: p
-    real(wp),dimension(3) :: f
+    real(wp),parameter :: s = sqrt(6378136.3D0**3/3986004.415D8)
 
     procedure(deriv_func) :: fcn
-    !! subroutine computing the value of \(dy/dx=f(x,y)\)
-    real(wp)              :: x  = 0.0_wp
-    !! `x` value (input is initial value and output is final value)
-    real(wp)              :: xf = 225000.0_wp
-    !! endpoint of integration (final value of `x`)
-    real(wp),dimension(6) :: y  = [1.0_wp,0.0_wp,0.0_wp,0.0_wp,1.0_wp,0.0_wp]
-    !! `y` value (input is initial value and output is final value)    
-!-------------------------------------------------------------------------------
+    real(wp)              :: t
+    real(wp),dimension(6) :: pv    
+
     call readecho(len_data_1961, t_1961, pxyz_1961, vxyz_1961)
 
-    print *, 'readecho ok!'
+    t = t_1961(1)
+    pv(1:3) = pxyz_1961(:,1)
+    pv(4:6) = vxyz_1961(:,1)
+    print *, pv
+    call easydop853(fcn, t, t+(len_data_1961-1)*86400/s, pv)
+    print *, pv
 
-    call readegm(n_max, c_matrix, s_matrix)
-
-    print *, 'readegm ok!'
-
-    call getabcdep(n_max, a, b, c, d, e, p)
-
-    print *, 'getabcdep ok!'
-
-    call getfe(36294.0D0, 2.0_wp**(1.0_wp/3.0_wp), 0.0_wp, 0.0_wp, &
-               n_max, a, b, c, d, e, c_matrix, s_matrix, p, f)
-
-    print *, sum(f**2.0_wp)**0.5_wp
-
-    call getfp(2436294.5_wp, [8000000.0_wp/6378136.3_wp,0.0_wp,0.0_wp], f)
-
-    print *, sum(f**2.0_wp)**0.5_wp
-
-    call easydop853(fcn, x, xf, y)
-
-    print *, x
-    print *, y
+    print *, pxyz_1961(:,len_data_1961)
+    print *, vxyz_1961(:,len_data_1961)
 
     end program main
