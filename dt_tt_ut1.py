acbf1b649e9ac308ea8229c47628c913f7015244
GasinAn <Gasin185@163.com>
2021-03-30 21:35:36 +0800

    new dt (nasa)

--- dt_tt_ut1.py
+++ dt_tt_ut1.py
@@ -3,12 +3,12 @@ from astropy.time import Time
 from matplotlib import pyplot as plt
 
 def mjd2y(mjd):
-    y = 1961+(mjd-(37300+365.25/24))/365.25
+    y = 1961+(mjd-37300)/365.25
     return y
 
 def dt_1961_1986(y):
     dy = y-1975
-    dt = 45.45+1.067*dy-3.84615e-3*dy**2-1.13927577e-3*dy**3
+    dt = 45.45+1.067*dy-dy**2/260-dy**3/718
     dt = dt/86400
     return dt
 
@@ -16,5 +16,5 @@ mjd = np.arange(41685, 46434)
 t = Time(mjd, format='mjd', scale='tt')
 tt, dt_tt_ut1, dt = t.tt.mjd, t.tt.mjd-t.ut1.mjd, dt_1961_1986(mjd2y(mjd))
 print(np.std((dt-dt_tt_ut1)*86400, ddof=1))
-plt.plot(tt, dt_tt_ut1, tt, dt)
+plt.plot(tt, dt_tt_ut1*86400, tt, dt*86400)
 plt.show()
--- tt2ut1.f90
+++ tt2ut1.f90
@@ -10,12 +10,20 @@
 
         real(wp) :: y, dy, dt
 
-        y = 1961+(ttmjd-(37300+365.25_wp/24))/365.25_wp
+        !y = 1961+(ttmjd-37300)/365.25
 
-        dy = y-1950
+        !dy = y-1950
+        !dy = y-1975
 
-        dt = 29.07_wp+0.407_wp*dy-4.2918e-3_wp*dy**3+3.926187e-4*dy**4
-        ut1mjd = ttmjd+dt/86400
+        !dt = 29.07+0.407*dy-dy**2/223+dy**3/2547
+        !dt = 45.45+1.067*dy-dy**2/260-dy**3/718
+        dt = 33.2_wp+(35.7_wp-33.2_wp)*((ttmjd-36934)/(38761-36934))
+
+        ut1mjd = ttmjd-dt/86400
+        !ut1mjd = ttmjd-(dt+0.2_wp)/86400
+        !ut1mjd = ttmjd-(dt-0.2_wp)/86400
+        !ut1mjd = ttmjd-33.2_wp/86400
+        !ut1mjd = ttmjd-35.7_wp/86400
         !ut1mjd = ttmjd
 
     end subroutine tt2ut1
